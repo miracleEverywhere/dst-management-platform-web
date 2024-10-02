5caa91c9440405ec4efeee04915f228789b2d644
Miracle <763483966@qq.com>
2024-10-02 17:02:35 +0800

    重构房间设置

--- src/api/setting/index.js
+++ src/api/setting/index.js
@@ -2,8 +2,8 @@ import http from "@/utils/axios.ts";
 
 
 export default {
-  base: {
-    url: `/setting/room/base`,
+  room: {
+    url: `/setting/room`,
     get: async function(data){
       return await http.get(this.url, data);
     },
@@ -11,31 +11,5 @@ export default {
       return await http.post(this.url, data);
     }
   },
-  ground: {
-    url: `/setting/room/ground`,
-    get: async function(data){
-      return await http.get(this.url, data);
-    },
-    post: async function (data) {
-      return await http.post(this.url, data);
-    }
-  },
-  cave: {
-    url: `/setting/room/cave`,
-    get: async function(data){
-      return await http.get(this.url, data);
-    },
-    post: async function (data) {
-      return await http.post(this.url, data);
-    }
-  },
-  mod: {
-    url: `/setting/room/mod`,
-    get: async function(data){
-      return await http.get(this.url, data);
-    },
-    post: async function (data) {
-      return await http.post(this.url, data);
-    }
-  },
+
 }
--- src/views/settings/room.vue
+++ src/views/settings/room.vue
@@ -13,26 +13,217 @@
         </el-card>
       </el-col>
     </el-row>
-    <room-base v-if="step===0" v-model:step="step"/>
-    <room-ground v-if="step===1" v-model:step="step"/>
-    <room-cave v-if="step===2" v-model:step="step"/>
-    <room-mod v-if="step===3" v-model:step="step"/>
-    <room-finish v-if="step===4" v-model:step="step"/>
+    <el-row :gutter="10" style="margin-top: 5px">
+      <el-col :span="24" :lg="24" :md="24" :sm="24" :xs="24">
+        <el-card v-if="step===0" shadow="never" :style="isMobile?'min-height: 400px':'min-height: 800px'" >
+          <el-form ref="roomBaseFormRef" :model="roomBaseForm" :rules="roomBaseFormRules" :label-width="isMobile?'70':'100'" :size="isMobile?'small':'large'">
+            <el-form-item label="房间名" prop="name">
+              <el-input v-model="roomBaseForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="房间描述" prop="description">
+              <el-input v-model="roomBaseForm.description"></el-input>
+            </el-form-item>
+            <el-form-item label="玩家对战" prop="pvp">
+              <el-switch v-model="roomBaseForm.pvp" />
+            </el-form-item>
+            <el-form-item label="玩家数量" prop="playerNum">
+              <el-slider v-model="roomBaseForm.playerNum" size="small" show-input :min="2" :max="100"/>
+            </el-form-item>
+            <el-form-item label="回档天数" prop="backDays">
+              <el-slider v-model="roomBaseForm.backDays" size="small" show-input  :min="5" :max="50"/>
+            </el-form-item>
+            <el-form-item label="玩家投票" prop="vote">
+              <el-switch v-model="roomBaseForm.vote" />
+            </el-form-item>
+            <el-form-item label="房间密码" prop="password">
+              <el-input v-model="roomBaseForm.password" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="令牌" prop="token">
+              <el-input v-model="roomBaseForm.token" show-password></el-input>
+              <div class="el-form-item-msg">
+                <el-link :underline="false" target="_blank"
+                         href="https://accounts.klei.com">点击获取游戏令牌</el-link>
+              </div>
+            </el-form-item>
+          </el-form>
+        </el-card>
+        <el-card v-if="step===1" shadow="never" :style="isMobile?'min-height: 400px':'min-height: 800px'">
+          <el-form ref="roomGroundFormRef" :model="roomGroundForm" :rules="roomGroundFormRules" :label-width="isMobile?'70':'100'" :size="isMobile?'small':'large'">
+            <el-form-item label-position="top" prop="groundSetting">
+              <sc-code-editor ref="editor" v-model="roomGroundForm.groundSetting" mode="lua" :height="isMobile?320:700" style="width: 100%"></sc-code-editor>
+            </el-form-item>
+          </el-form>
+        </el-card>
+        <el-card v-if="step===2" shadow="never" :style="isMobile?'min-height: 400px':'min-height: 800px'">
+          <el-form ref="roomCaveFormRef" :model="roomCaveForm" :rules="roomCaveFormRules" :label-width="isMobile?'70':'100'" :size="isMobile?'small':'large'">
+            <el-form-item label-position="top" prop="caveSetting">
+              <sc-code-editor ref="editor" v-model="roomCaveForm.caveSetting" mode="lua" :height="isMobile?320:700" style="width: 100%"></sc-code-editor>
+            </el-form-item>
+          </el-form>
+        </el-card>
+        <el-card v-if="step===3" shadow="never" :style="isMobile?'min-height: 400px':'min-height: 800px'">
+          <el-form ref="roomModFormRef" :model="roomModForm" :rules="roomModFormRules" :label-width="isMobile?'70':'100'" :size="isMobile?'small':'large'">
+            <el-form-item label-position="top" prop="modSetting">
+              <sc-code-editor ref="editor" v-model="roomModForm.modSetting" mode="lua" :height="isMobile?320:700" style="width: 100%"></sc-code-editor>
+            </el-form-item>
+          </el-form>
+        </el-card>
+        <el-card v-if="step===4" shadow="never" :style="isMobile?'min-height: 400px':'min-height: 800px'">
+          <el-result
+            icon="success"
+            title="Success Tip"
+            sub-title="Please follow the instructions"
+            style="margin-top: 20%"
+          >
+          </el-result>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col :span="24" :lg="24" :md="24" :sm="24" :xs="24">
+        <el-card shadow="never">
+          <div style="display: flex; justify-content: flex-end;">
+            <el-button v-if="step>0" @click="handlePrev">上一步</el-button>
+            <el-button v-if="step<4" type="primary" @click="handleNext">下一步</el-button>
+            <el-button v-if="step===4" type="success" @click="handleSave">仅保存</el-button>
+            <el-button v-if="step===4" type="warning" @click="handleSaveAndRestart">保存并重启</el-button>
+            <el-button v-if="step===4" type="danger" @click="handleGenerateNewWorld">生成新世界</el-button>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup>
-import RoomBase from './components/room-base.vue'
-import RoomGround from './components/room-ground.vue'
-import RoomCave from './components/room-cave.vue'
-import RoomMod from './components/room-mod.vue'
-import RoomFinish from './components/room-finish.vue'
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {useScreenStore} from "@/hooks/screen/index.ts";
+import scCodeEditor from "@/components/scCodeEditor/index.vue";
+import settingApi from "@/api/setting"
+import luaparse from 'luaparse'
+import {koiMsgError, koiMsgSuccess} from "@/utils/koi.ts";
+
+
+onMounted(() => {
+  handleGetCurrentRoomSetting()
+})
 
 const { isMobile } = useScreenStore();
 
 const step = ref(0)
+const handlePrev = () => {
+  step.value--
+}
+const handleNext = () => {
+  if (step.value === 0) {
+    roomBaseFormRef.value.validate(valid => {
+      if (valid) {
+        step.value++
+      }
+    })
+  } else if (step.value === 1) {
+    roomGroundFormRef.value.validate(valid => {
+      if (valid) {
+        try {
+          luaparse.parse(roomGroundForm.value.groundSetting);
+          step.value++
+        } catch (e) {
+          koiMsgError('lua语法校验失败')
+        }
+      }
+    })
+  } else if (step.value === 2) {
+    roomCaveFormRef.value.validate(valid => {
+      if (valid) {
+        try {
+          luaparse.parse(roomCaveForm.value.caveSetting);
+          step.value++
+        } catch (e) {
+          koiMsgError('lua语法校验失败')
+        }
+      }
+    })
+  } else if (step.value === 3) {
+    roomModFormRef.value.validate(valid => {
+      if (valid) {
+        try {
+          luaparse.parse(roomModForm.value.modSetting);
+          step.value++
+        } catch (e) {
+          koiMsgError('lua语法校验失败')
+        }
+      }
+    })
+  }
+}
+
+const roomBaseFormRef = ref()
+const roomBaseForm = ref({
+  name: '',
+  description: '',
+  pvp: false,
+  playerNum: 6,
+  backDays: 10,
+  vote: true,
+  password: '',
+  token: '',
+})
+const roomBaseFormRules = {
+  name: [ { required: true, message: 'Please input Activity name', trigger: 'blur' } ],
+  token: [ { required: true, message: 'Please input Activity name', trigger: 'blur' } ],
+}
+
+const roomGroundFormRef = ref()
+const roomGroundForm = ref({
+  groundSetting: '',
+})
+const roomGroundFormRules = {
+  groundSetting: [ { required: true, message: 'Please input Activity name', trigger: 'blur' } ],
+}
+
+const roomCaveFormRef = ref()
+const roomCaveForm = ref({
+  caveSetting: '',
+})
+const roomCaveFormRules = {
+  caveSetting: [ { required: true, message: 'Please input Activity name', trigger: 'blur' } ],
+}
+
+const roomModFormRef = ref()
+const roomModForm = ref({
+  modSetting: '',
+})
+const roomModFormRules = {
+  modSetting: [ { required: true, message: 'Please input Activity name', trigger: 'blur' } ],
+}
+
+
+const handleGetCurrentRoomSetting = () => {
+  settingApi.room.get().then(response => {
+    roomBaseForm.value = response.data.base
+    roomGroundForm.value.groundSetting = response.data.ground
+    roomCaveForm.value.caveSetting = response.data.cave
+    roomModForm.value.modSetting = response.data.mod
+  })
+}
+
+const handleSave = () => {
+  const reqForm = {
+    base: roomBaseForm.value,
+    ground: roomGroundForm.value.groundSetting,
+    cave: roomCaveForm.value.caveSetting,
+    mod: roomModForm.value.modSetting
+  }
+  settingApi.room.post(reqForm).then(response => {
+    koiMsgSuccess(response.message)
+  })
+}
+const handleSaveAndRestart = () => {
+
+}
+const handleGenerateNewWorld = () => {
+
+}
 
 </script>
 
