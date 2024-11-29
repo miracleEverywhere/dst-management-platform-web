5527d08cc215797655c2a0ec4384f6d302a9f7be
Miracle <763483966@qq.com>
2024-11-29 21:05:06 +0800

    可视化组件

--- src/views/settings/components/levelDataSetting.vue
+++ src/views/settings/components/levelDataSetting.vue
@@ -7,7 +7,7 @@
       </div>
       <div style="margin: 5px 0" class="fcc">
         <el-button :icon="ArrowLeftBold" link :disabled="leftClickDisabled" @click="leftClick"></el-button>
-        <el-tag size="large" effect="plain" type="primary" style="margin: 0 5px">{{language==='zh'?configsMap[setting]:setting}}</el-tag>
+        <el-tag size="large" effect="plain" type="primary" style="margin: 0 5px">{{language==='zh'?configsMap[setting].zh:configsMap[setting].en}}</el-tag>
         <el-button :icon="ArrowRightBold" link :disabled="rightClickDisabled" @click="rightClick"></el-button>
       </div>
     </div>
@@ -17,7 +17,7 @@
 
 <script setup>
 import {ArrowLeftBold, ArrowRightBold} from '@element-plus/icons-vue'
-import {computed, ref} from "vue";
+import {computed, ref, watch} from "vue";
 import useGlobalStore from "@/stores/modules/global.ts";
 import {configsMap, overrides} from "@/views/settings/components/levelDataMap.js"
 
@@ -30,6 +30,7 @@ const props = defineProps({
 })
 
 
+
 const globalStore = useGlobalStore();
 const language = computed(() => globalStore.language);
 
@@ -39,26 +40,11 @@ const setting = ref(props.modelValue)
 
 const leftClick = () => {
   const index = props.configs.indexOf(setting.value)
-  console.log(index)
-  if (index === 1) {
-    setting.value = props.configs[index - 1]
-    leftClickDisabled.value = true
-  } else {
-    rightClickDisabled.value = false
-    setting.value = props.configs[index - 1]
-  }
-  handleSettingChange()
+  setting.value = props.configs[index - 1]
 }
 const rightClick = () => {
   const index = props.configs.indexOf(setting.value)
-  if (index === (props.configs.length - 2)) {
-    setting.value = props.configs[index + 1]
-    rightClickDisabled.value = true
-  } else {
-    leftClickDisabled.value = false
-    setting.value = props.configs[index + 1]
-  }
-  handleSettingChange()
+  setting.value = props.configs[index + 1]
 }
 
 const getImageUrl = (file) => {
@@ -67,10 +53,23 @@ const getImageUrl = (file) => {
 
 const handleSettingChange = () => {
   overrides[props.name].modelValue = setting.value
-  console.log(overrides)
   //TODO 回写lua文件
 
 }
+
+
+watch(() => setting.value, (newValue, oldValue) => {
+  const index = props.configs.indexOf(setting.value)
+  if (index === 0) {
+    leftClickDisabled.value = true
+  } else if (index === (props.configs.length - 1)) {
+    rightClickDisabled.value = true
+  } else {
+    leftClickDisabled.value = false
+    rightClickDisabled.value = false
+  }
+  handleSettingChange()
+}, {immediate: true})
 
 </script>
 
