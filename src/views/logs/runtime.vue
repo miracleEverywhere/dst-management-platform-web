8bc59ed98dfe97f35930df5ea3ccc4a9d0f65493
Miracle <763483966@qq.com>
2024-12-03 15:54:36 +0800

    新增runtime日志功能

---
+++ src/views/logs/runtime.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-div">
+    <el-row :gutter="10">
+      <el-col :span="24" :lg="24" :md="24" :sm="24" :xs="24" style="margin-top: 10px">
+        <log type="runtime"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import log from "./components/log.vue"
+
+</script>
+
+<style scoped>
+</style>
