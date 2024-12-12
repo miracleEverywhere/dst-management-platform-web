01cb2f29942c49e2f28194f340d27786598b873d
Miracle <763483966@qq.com>
2024-12-12 17:50:47 +0800

    更新mod配置

--- src/views/mod/index.vue
+++ src/views/mod/index.vue
@@ -1,15 +1,69 @@
 <template>
+  <div class="page-div">
+    <el-tabs v-model="activeTabName">
+      <el-tab-pane label="Setting" name="Setting">
+        <el-row :gutter="10">
+          <el-col :lg="8" :md="8" :sm="24" :span="8" :xs="24">
+            <el-card v-loading="modSettingFormatLoading" :style="isMobile?'height: 50vh':'height: 70vh'" shadow="never">
+              <el-scrollbar :max-height="isMobile?'45vh':'65vh'">
+                <template v-for="(mod, index) in modSettingFormat">
+                  <div style="display: flex">
+                    <div>
+                      <el-image :src="mod.preview_url" fit="fill" @click="handleModClick(mod.id)" style="width: 75px; height: 75px"/>
+                    </div>
+                    <div style="display: flex;margin-left: 5px;flex-direction: column;justify-content: center">
+                      <el-button link type="primary" @click="handleModClick(mod.id)">{{ mod.name }}</el-button>
+                      <div>
+                        <el-tag v-if="mod.enable" type="success">启用</el-tag>
+                        <el-tag v-if="!mod.enable" type="info">禁用</el-tag>
+                      </div>
+                    </div>
+                  </div>
+                  <el-divider v-if="index !== (modSettingFormat.length - 1)" />
+                </template>
+              </el-scrollbar>
+            </el-card>
+          </el-col>
+          <el-col :lg="16" :md="16" :sm="24" :span="16" :xs="24">
+            <el-card :style="isMobile?'height: 50vh; margin-top: 10px':'height: 70vh'" shadow="never">
+              <template v-if="clickedModID!==0">
+                <el-scrollbar :max-height="isMobile?'45vh':'65vh'">
+                  <el-form ref="modSettingFormRef" :size="isMobile?'small':'large'"
+
+                           :label-position="isMobile?'top':'left'" :label-width="isMobile?'70':'auto'">
+                    <template v-for="item in modConfigurations.configOptions">
+                      <el-form-item :label="item.label">
+                        <el-select v-model="modSettingFormat[modSettingFormat.findIndex(item => item.id === clickedModID)].configurationOptions">
+                          <template v-for="i in item.options">
+                            <el-option :label="i.description" :value="i.data"/>
+                          </template>
+                        </el-select>
+                      </el-form-item>
+                    </template>
+                  </el-form>
+                  {{modSettingFormat[modSettingFormat.findIndex(item => item.id === clickedModID)].configurationOptions}}
+                </el-scrollbar>
+              </template>
+              <template v-if="clickedModID===0">
+                <el-result icon="info" title="请选择一个模组进行配置"/>
+              </template>
+            </el-card>
+          </el-col>
+
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="Download" name="Download">
+      </el-tab-pane>
+      <el-tab-pane label="Subscription" name="Subscription">
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
-<script setup name="mod">
+<script name="mod" setup>
 import modApi from "@/api/mod"
-import {computed, onMounted, ref, watch} from "vue";
+import {computed, onMounted, ref} from "vue";
 import {useScreenStore} from "@/hooks/screen/index.ts";
-import scCodeEditor from "@/components/scCodeEditor/index.vue";
-import settingApi from "@/api/setting"
-import luaparse from 'luaparse'
-import luamin from 'lua-format'
-import {koiMsgError, koiMsgSuccess} from "@/utils/koi.ts";
 import {useI18n} from "vue-i18n";
 import useGlobalStore from "@/stores/modules/global.ts";
 
@@ -23,25 +77,38 @@ const {isMobile} = useScreenStore();
 const globalStore = useGlobalStore();
 const isDark = computed(() => globalStore.isDark);
 
-modApi.info.get()
+const activeTabName = ref('Setting')
 
-const modSetting = ref('')
+const modSettingFormat = ref([])
+const modSettingFormatLoading = ref(false)
 const handleGetModSetting = () => {
-  modApi.setting.get().then(response => {
-    modSetting.value = response.data
-    let a = "3"
-    console.log(a)
-    console.log(eval(a))
+  modSettingFormatLoading.value = true
+  modApi.settingFormat.get().then(response => {
+    modSettingFormat.value = response.data
+  }).finally(() => {
+    modSettingFormatLoading.value = false
   })
 }
 
-const modObj = ref({})
+const clickedModID = ref(0)
+// const clickedModSetting = ref({})
+const handleModClick = (modID) => {
+  clickedModID.value = modID
+  // clickedModSetting.value = modSettingFormat.value.find(item => item.id === clickedModID.value).configurationOptions
+  handleGetModConfigurations()
+}
 
+const modConfigurationsLoading = ref(false)
+const modConfigurations = ref({})
+const handleGetModConfigurations = () => {
+  modConfigurationsLoading.value = true
+  modApi.configOptions.get({id: clickedModID.value}).then(response => {
+    modConfigurations.value = response.data
+  }).finally(() => {
+    modConfigurationsLoading.value = false
+  })
+}
 
-
-watch(() => modSetting.value, (newValue, oldValue) => {
-
-}, {immediate: false})
 </script>
 
 <style scoped>
