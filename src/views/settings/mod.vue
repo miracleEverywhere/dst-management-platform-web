e611b2ccaec3957226457254bb1ee195e37dadcf
Miracle <763483966@qq.com>
2024-12-25 18:42:13 +0800

    修复无mod时无法添加mod的bug

--- src/views/settings/mod.vue
+++ src/views/settings/mod.vue
@@ -91,7 +91,7 @@
           <el-col :lg="8" :md="8" :sm="24" :span="8" :xs="24">
             <el-card v-loading="modSettingFormatLoading" :style="isMobile?'height: 50vh':'height: 70vh'" shadow="never">
               <el-scrollbar :max-height="isMobile?'45vh':'65vh'">
-                <template v-if="modSettingFormat.length !== 0">
+                <template v-if="modSettingFormat">
                   <template v-for="(mod, index) in modSettingFormat">
                     <div style="display: flex">
                       <div>
