6f417d0c34d515e8028b74b62795ef7a81237661
Miracle <763483966@qq.com>
2025-01-21 18:16:38 +0800

    日志清理页面

---
+++ src/views/logs/clean.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-div">
+    <el-row :gutter="10">
+      <el-col :lg="24" :md="24" :sm="24" :span="24" :xs="24" style="margin-top: 10px">
+        <el-card shadow="never" :style="isMobile?'min-height: 500px':'min-height: 700px'">
+          <template #header>
+            <div class="card-header">
+            </div>
+          </template>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
