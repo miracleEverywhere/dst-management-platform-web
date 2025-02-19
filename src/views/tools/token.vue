10ca6bce0fc78be60037c24e8d9a069ce4e99b0f
Miracle <763483966@qq.com>
2025-02-19 15:33:17 +0800

    新增示例代码

--- src/views/tools/token.vue
+++ src/views/tools/token.vue
@@ -35,7 +35,7 @@
             <div>
               {{ t('tools.token.usage') }}
             </div>
-            <MdPreview :modelValue="requestPython"
+            <MdPreview :modelValue="requestShow"
                        :theme="isDark?'dark':'light'"
                        previewTheme="github"/>
           </div>
@@ -82,7 +82,7 @@ const handleCreateToken = () => {
   })
 }
 
-const requestPython = ref('```python\n' +
+const requestPython = ref('```python [id:Python]\n' +
   'import requests\n' +
   '\n' +
   'url = "http://{ip}:{port}"\n' +
@@ -102,7 +102,116 @@ const requestPython = ref('```python\n' +
   '\n' +
   'print(response.text)\n' +
   '```');
+const requestGolang = ref('```golang [id:Golang]\n' +
+  'package main\n' +
+  '\n' +
+  'import (\n' +
+  '  "fmt"\n' +
+  '  "net/http"\n' +
+  '  "io"\n' +
+  ')\n' +
+  '\n' +
+  'func main() {\n' +
+  '  token := "your token"\n' +
+  '  url := "http://{ip}:{port}"\n' +
+  '  method := "GET"\n' +
+  '  //中文\n' +
+  '  lang := "zh"\n' +
+  '  //English\n' +
+  '  //lang := "en"\n' +
+  '\n' +
+  '  client := &http.Client{}\n' +
+  '  req, err := http.NewRequest(method, url, nil)\n' +
+  '\n' +
+  '  if err != nil {\n' +
+  '    fmt.Println(err)\n' +
+  '    return\n' +
+  '  }\n' +
+  '  req.Header.Add("Authorization", token)\n' +
+  '  req.Header.Add("X-I18n-Lang", lang)\n' +
+  '\n' +
+  '  res, err := client.Do(req)\n' +
+  '  if err != nil {\n' +
+  '    fmt.Println(err)\n' +
+  '    return\n' +
+  '  }\n' +
+  '  defer res.Body.Close()\n' +
+  '\n' +
+  '  body, err := io.ReadAll(res.Body)\n' +
+  '  if err != nil {\n' +
+  '    fmt.Println(err)\n' +
+  '    return\n' +
+  '  }\n' +
+  '  fmt.Println(string(body))\n' +
+  '}\n' +
+  '```')
+const requestJava = ref('```java [id:Java]\n' +
+  'import java.io.BufferedReader;\n' +
+  'import java.io.InputStreamReader;\n' +
+  'import java.net.HttpURLConnection;\n' +
+  'import java.net.URL;\n' +
+  '\n' +
+  'public class Main {\n' +
+  '    public static void main(String[] args) {\n' +
+  '        try {\n' +
+  '            // 定义请求的 URL\n' +
+  '            String url = "http://{ip}:{port}";\n' +
+  '            // 定义 token 和语言\n' +
+  '            String token = "your token";\n' +
+  '            String lang = "zh"; // 中文\n' +
+  '            // String lang = "en"; // English\n' +
+  '\n' +
+  '            // 创建 URL 对象\n' +
+  '            URL apiUrl = new URL(url);\n' +
+  '            // 打开连接\n' +
+  '            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();\n' +
+  '            // 设置请求方法\n' +
+  '            connection.setRequestMethod("GET");\n' +
+  '            // 添加请求头\n' +
+  '            connection.setRequestProperty("Authorization", token);\n' +
+  '            connection.setRequestProperty("X-I18n-Lang", lang);\n' +
+  '\n' +
+  '            // 获取响应码\n' +
+  '            int responseCode = connection.getResponseCode();\n' +
+  '            System.out.println("Response Code: " + responseCode);\n' +
+  '\n' +
+  '            // 读取响应内容\n' +
+  '            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));\n' +
+  '            String inputLine;\n' +
+  '            StringBuilder response = new StringBuilder();\n' +
+  '\n' +
+  '            while ((inputLine = in.readLine()) != null) {\n' +
+  '                response.append(inputLine);\n' +
+  '            }\n' +
+  '            in.close();\n' +
+  '\n' +
+  '            // 打印响应内容\n' +
+  '            System.out.println("Response Body: " + response.toString());\n' +
+  '        } catch (Exception e) {\n' +
+  '            e.printStackTrace();\n' +
+  '        }\n' +
+  '    }\n' +
+  '}\n' +
+  '```')
+const requestCurl = ref('```bash [id:cURL]\n' +
+  'curl --location --globoff \'http://{ip}:{port}\' \\\n' +
+  '--header \'Authorization: token\' \\\n' +
+  '--header \'X-I18n-Lang: lang\'\n' +
+  '```')
+const requestPowerShell = ref('```powershell [id:PowerShell]\n'+
+  '$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"\n' +
+  '$headers.Add("Authorization", "token")\n' +
+  '$headers.Add("X-I18n-Lang", "lang")\n' +
+  '\n' +
+  '$response = Invoke-RestMethod \'http://{ip}:{port}\' -Method \'GET\' -Headers $headers\n' +
+  '$response | ConvertTo-Json\n' +
+  '```')
 
+const requestShow = requestPython.value + '\n\n' +
+  requestGolang.value + '\n\n' +
+  requestJava.value + '\n\n' +
+  requestCurl.value + '\n\n' +
+  requestPowerShell.value
 </script>
 
 <style scoped>
