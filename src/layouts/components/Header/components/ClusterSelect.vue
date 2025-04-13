c4aad6d8292e6da0cf774bcda2e9b502b9227ce3
Miracle <763483966@qq.com>
2025-04-13 14:09:01 +0800

    重构

--- src/layouts/components/Header/components/ClusterSelect.vue
+++ src/layouts/components/Header/components/ClusterSelect.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <el-select v-model="globalStore.selectedDstCluster" size="small" style="width: 100px">
+    <el-select v-model="globalStore.selectedDstCluster" @change="handleClusterChange" size="small" style="width: 100px">
       <el-option v-for="cluster in globalStore.dstClusters" :label="cluster.clusterName"
                  :value="cluster.clusterName"/>
-      <el-option label="新建" value="new"/>
+      <el-option label="新建" :value="null" @click="handleClusterNew"/>
     </el-select>
+    <el-dialog v-model="newClusterDialog">
+      <template #header>
+        新建集群
+      </template>
+      <el-form>
+        <el-form-item label="集群名">
+          <el-input>
+
+          </el-input>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
-import {computed, onMounted} from "vue";
+import {computed, onMounted, ref} from "vue";
 import settingApi from '@/api/setting'
 import useGlobalStore from "@/stores/modules/global.ts";
 
@@ -27,6 +39,18 @@ const getClusters = () => {
     globalStore.dstClusters = response.data
   })
 }
+
+const handleClusterChange = () => {
+  if (globalStore.selectedDstCluster !== null) {
+
+  }
+}
+
+const handleClusterNew = () => {
+  newClusterDialog.value = true
+}
+
+const newClusterDialog = ref(false)
 </script>
 
 <style scoped>
