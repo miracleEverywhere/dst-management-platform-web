3b72e800abf64b87a1f9abf6aea1d4ec7e9bcff7
miracleEverywhere <763483966@qq.com>
2025-05-22 11:45:35 +0800

    新增获取所有集群接口

--- src/api/setting/index.js
+++ src/api/setting/index.js
@@ -7,6 +7,12 @@ export default {
     get: async function(data){
       return await http.get(this.url, data);
     },
+    all: {
+      url: `/setting/clusters/all`,
+      get: async function(data){
+        return await http.get(this.url, data);
+      },
+    },
   },
   clustersWorldPort: {
     url: `/setting/clusters/world_port`,
