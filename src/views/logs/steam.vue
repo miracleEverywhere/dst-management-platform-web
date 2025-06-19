ebe15e5c4aae53c27688e8279d72ca4a68328339
miracleEverywhere <763483966@qq.com>
2025-06-19 10:10:07 +0800

    新增steam日志

---
+++ src/views/logs/steam.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-div">
+    <el-row :gutter="10">
+      <el-col :lg="24" :md="24" :sm="24" :span="24" :xs="24" style="margin-top: 10px">
+        <log type="steam"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import log from "./components/log.vue"
+</script>
+
+<style scoped>
+</style>
