9315c44ccd5537c6de25b2ac1c6077d7abfb5ea3
miracleEverywhere <763483966@qq.com>
2025-09-01 19:08:28 +0800

    绘制关键物体

--- src/views/tools/summary.vue
+++ src/views/tools/summary.vue
@@ -1,5 +1,21 @@
 <template>
-  <el-image :src="'data:image/png;base64,'+imageData.image" alt="Base64图片" :style="'width: '+imageData.width*2+'px; height: '+imageData.height*2+'px'"/>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <span>世界统计</span>
+        <el-select v-model="world" @change="getSummary"
+                   style="width: 10vw; margin-right: 10px; font-weight: lighter">
+          <el-option v-for="world in globalStore.dstClusters.find(cluster => cluster.clusterName === globalStore.selectedDstCluster).worlds"
+                     :label="world" :value="world"></el-option>
+        </el-select>
+      </div>
+    </template>
+    <div>
+      <canvas ref="canvas"></canvas>
+      {{Data.count}}
+    </div>
+  </el-card>
+
 </template>
 
 <script setup>
@@ -10,24 +26,73 @@ import useGlobalStore from "@/stores/modules/global.ts";
 
 const globalStore = useGlobalStore();
 
-onMounted(() => {
-  getSummary()
+onMounted(async () => {
+  await getSummary()
 })
 
-const imageData = ref({
-  height: 0,
-  width: 0,
-  image: ''
+const Data = ref({
+  image: {
+    height: 0,
+    width: 0,
+    image: '',
+  },
+  prefabs: [],
+  count: []
 })
-const getSummary = () => {
+
+const canvas = ref()
+const world = ref(globalStore.dstClusters?.find(cluster => cluster.clusterName === globalStore.selectedDstCluster)?.worlds?.[0] ?? null)
+
+const getSummary = async () => {
   const reqForm = {
     clusterName: globalStore.selectedDstCluster,
-    worldName: "World1"
+    worldName: world.value
   }
-  toolsApi.summary.get(reqForm).then(response => {
-    imageData.value = response.data
+  toolsApi.summary.get(reqForm).then(async response => {
+    Data.value = response.data
+    await generateImage()
   })
 }
+
+
+
+const generateImage = async () => {
+  const mainImage = new Image();
+  mainImage.src = 'data:image/png;base64,' + Data.value.image.image;
+
+  const stickerImages = []
+
+  for (let item of Data.value.prefabs) {
+    const image = new Image()
+    image.src = new URL(`./images/${item.name}.png`, import.meta.url).href
+    stickerImages.push(image)
+  }
+
+  // 等待所有图片加载完成
+  await Promise.all([
+    new Promise((resolve) => { mainImage.onload = resolve; }),
+    Promise.all(stickerImages.map(image =>
+      new Promise(resolve => {
+        image.onload = resolve
+      })
+    ))
+  ]);
+
+  const ctx = canvas.value.getContext('2d');
+
+  // 使用实际图片尺寸
+  canvas.value.width = Data.value.image.width;
+  canvas.value.height = Data.value.image.height;
+
+  // 绘制主图（使用实际尺寸）
+  await ctx.drawImage(mainImage, 0, 0, Data.value.image.width, Data.value.image.height);
+
+  for (let index = 0; index < stickerImages.length; index++) {
+    await ctx.drawImage(stickerImages[index], Data.value.prefabs[index].x-8, Data.value.prefabs[index].y-8, 16, 16);
+  }
+}
+
+
 </script>
 
 <style scoped>
