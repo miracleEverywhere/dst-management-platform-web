f899a618c743ab0cbc45b6f7032092c77f9bc646
miracleEverywhere <763483966@qq.com>
2025-10-28 23:15:27 +0800

    哦豁

--- src/layouts/components/NavHeader.vue
+++ src/layouts/components/NavHeader.vue
@@ -2,7 +2,7 @@
   <v-img
     width="3em"
     height="2em"
-    src="src/assets/images/dmp.svg"
+    :src="logo"
   />
 
   <h1 class="font-weight-medium leading-normal text-xl text-uppercase">
@@ -10,5 +10,17 @@
   </h1>
 </template>
 
+<script setup>
+import {generateLogo} from "@/utils/logo.js";
+const logo = generateLogo()
+</script>
+
+<style scoped>
+
+.logo {
+  width: 3em!important;
+  height: 2em!important;
+}
+</style>
 
 
