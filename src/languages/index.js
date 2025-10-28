0ea630ff6036295c5f528d32f77cc1e0d93c8859
miracleEverywhere <763483966@qq.com>
2025-10-28 10:55:33 +0800

    优化登录页面

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,6 +1,6 @@
 import { createI18n } from "vue-i18n"
-import zh from "@/languages/zh"
-import en from "@/languages/en"
+import zh from "@/languages/lang-zh"
+import en from "@/languages/lang-en"
 import { getBrowserLang } from "@/utils/tools"
 
 const i18n = createI18n({
---
+++ src/languages/lang-zh.js
@@ -0,0 +1,40 @@
+export default {
+  global: {
+    title: "饥荒管理平台",
+    github: "点击访问Github源码",
+    document: "查看文档",
+    light: "明亮",
+    dark: "黑暗",
+    profile: "个人中心",
+    logout: "退出平台",
+  },
+  menu: {
+    dashboard: "控制台",
+  },
+  login: {
+    welcome: "欢迎使用饥荒管理平台",
+    loginForm: {
+      username: "用户名",
+      password: "密码",
+    },
+    loginFormRule: {
+      username: "请输入用户名",
+      password: "请输入密码",
+    },
+    registerForm: {
+      username: "用户名",
+      nickname: "用户昵称",
+      password: "密码",
+    },
+    registerFormRule: {
+      username: "请输入用户名",
+      nickname: "请输入用户昵称",
+      password: "请输入密码",
+    },
+    login: "登录",
+    register: "注册",
+    cancel: "取消",
+    submit: "提交",
+    loginSuccess: "登录成功",
+  },
+}
