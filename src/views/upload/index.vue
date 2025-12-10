ecc2783c82cfdd9feabdfb6450ab1309daa85227
miracleEverywhere <763483966@qq.com>
2025-12-10 21:48:37 +0800

    优化存档导入

--- src/views/upload/index.vue
+++ src/views/upload/index.vue
@@ -1,18 +1,38 @@
 <template>
-  <v-dialog v-model="uploadDialogVisible" :persistent="uploadLoading" class="flex-wrap" max-width="65%">
-    <template v-slot:activator="{ props: activatorProps }">
-      <v-btn class="mr-2" color="primary" v-bind="activatorProps">上传存档</v-btn>
+  <v-dialog
+    v-model="uploadDialogVisible"
+    :persistent="uploadLoading"
+    class="flex-wrap"
+    :width="mobile?'90%':'40%'"
+  >
+    <template #activator="{ props: activatorProps }">
+      <v-btn
+        class="mr-2"
+        color="primary"
+        v-bind="activatorProps"
+      >
+        上传存档
+      </v-btn>
     </template>
-    <template v-slot:default="{ isActive }">
-      <v-card title="导入存档" min-height="500">
+    <template #default="{ isActive }">
+      <v-card
+        title="导入存档"
+        min-height="500"
+      >
         <v-card-text>
           <template v-if="!uploadLoading">
-            <v-alert color="warning" density="compact" class="mt-2 mb-2">
+            <v-alert
+              color="warning"
+              density="compact"
+              class="mt-2 mb-2"
+            >
               请上传.zip文件，请严格按照教程中的路径进行压缩，上传过程中请勿关闭本对话框
             </v-alert>
-            <v-file-upload density="default" icon="ri-upload-cloud-2-line"
-                           @update:modelValue="handleUpload">
-            </v-file-upload>
+            <v-file-upload
+              density="default"
+              icon="ri-upload-cloud-2-line"
+              @update:model-value="handleUpload"
+            />
           </template>
           <template v-else>
             上传中，请稍后
@@ -24,26 +44,34 @@
 </template>
 
 <script setup>
-import {showSnackbar} from "@/utils/snackbar.js";
+import { showSnackbar } from "@/utils/snackbar.js"
 import roomApi from "@/api/room.js"
+import { useDisplay } from "vuetify/framework"
 
-
+const { mobile } = useDisplay()
 const uploadLoading = ref(false)
 const uploadDialogVisible = ref(false)
-const checkUploadFile = (param) => {
-  const zipPattern = /\.zip$/i;
-  return zipPattern.test(param.name);
+
+const checkUploadFile = param => {
+  const zipPattern = /\.zip$/i
+  
+  return zipPattern.test(param.name)
 }
-const handleUpload = (file) => {
+
+const handleUpload = file => {
   if (!checkUploadFile(file)) {
     showSnackbar('请上传zip文件', 'error')
     uploadDialogVisible.value = false
+    
     return
   }
   uploadLoading.value = true
+
   const formData = new FormData()
+
   formData.append('file', file)
-  formData.append('roomID', 1)
+
+  // formData.append('roomID', 1)
   roomApi.upload.post(formData).then(response => {
     showSnackbar(response.message)
   }).finally(() => {
@@ -53,5 +81,3 @@ const handleUpload = (file) => {
 }
 </script>
 
-<style scoped>
-</style>
