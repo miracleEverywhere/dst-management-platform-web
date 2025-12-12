64bce45b53345245f1ebd790fb5afd3679d4099b
miracleEverywhere <763483966@qq.com>
2025-12-12 10:31:24 +0800

    组件化消息条

---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    :location="location"
+    :timeout="timeout"
+    :color="color"
+    transition="slide-y-transition"
+    variant="flat"
+  >
+    <v-icon :icon="icon" />
+    {{ text }}
+  </v-snackbar>
+</template>
+
+<script setup>
+import { useSnackbar } from "@/utils/snackbar.js"
+
+const { snackbar, color, text, location, icon, timeout } = useSnackbar()
+</script>
