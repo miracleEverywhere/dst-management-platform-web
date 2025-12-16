664805b5f744628985eb64b18550fd80a9b94ca0
miracleEverywhere <763483966@qq.com>
2025-12-16 16:58:54 +0800

    添加screen接口

--- src/api/platform.js
+++ src/api/platform.js
@@ -34,4 +34,24 @@ export default {
       return await http.post(this.url, data)
     },
   },
+  runningScreens: {
+    url: `/platform/running_screens`,
+    get: async function (data) {
+      return await http.get(this.url, data)
+    },
+  },
+  screen: {
+    running: {
+      url: `/platform/screen/running`,
+      get: async function (data) {
+        return await http.get(this.url, data)
+      },
+    },
+    kill: {
+      url: `/platform/screen/kill`,
+      post: async function (data) {
+        return await http.post(this.url, data)
+      },
+    },
+  },
 }
