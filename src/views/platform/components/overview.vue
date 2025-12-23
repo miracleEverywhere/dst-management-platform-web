2c6cb540a409aea3ef0b4e9e552ac9d28f76cf04
miracleEverywhere <763483966@qq.com>
2025-12-23 16:05:54 +0800

    添加监控数据汇总

--- src/views/platform/components/overview.vue
+++ src/views/platform/components/overview.vue
@@ -142,19 +142,115 @@
         </v-card-text>
       </v-card>
     </v-col>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <v-card>
+        <template #prepend>
+          <v-icon
+            color="primary"
+            class="me-8"
+            icon="ri-id-card-line"
+            size="48"
+          />
+          <span>
+            {{ t('platform.overview.maxCpu') }}
+          </span>
+        </template>
+        <v-card-text class="d-flex justify-end">
+          {{ overviewData.maxCpu?overviewData.maxCpu.toFixed(2):overviewData.maxCpu }} %
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <v-card>
+        <template #prepend>
+          <v-icon
+            color="primary"
+            class="me-8"
+            icon="ri-id-card-line"
+            size="48"
+          />
+          <span>
+            {{ t('platform.overview.maxMemory') }}
+          </span>
+        </template>
+        <v-card-text class="d-flex justify-end">
+          {{ overviewData.maxMemory?overviewData.maxMemory.toFixed(2):overviewData.maxMemory }} %
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <v-card>
+        <template #prepend>
+          <v-icon
+            color="primary"
+            class="me-8"
+            icon="ri-id-card-line"
+            size="48"
+          />
+          <span>
+            {{ t('platform.overview.maxNetUp') }}
+          </span>
+        </template>
+        <v-card-text class="d-flex justify-end">
+          {{ overviewData.maxNetUp?overviewData.maxNetUp.toFixed(2):overviewData.maxNetUp }} KB/s
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <v-card>
+        <template #prepend>
+          <v-icon
+            color="primary"
+            class="me-8"
+            icon="ri-id-card-line"
+            size="48"
+          />
+          <span>
+            {{ t('platform.overview.maxNetDown') }}
+          </span>
+        </template>
+        <v-card-text class="d-flex justify-end">
+          {{ overviewData.maxNetDown?overviewData.maxNetDown.toFixed(2):overviewData.maxNetDown }} KB/s
+        </v-card-text>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
 <script setup>
-import platformApi from "@/api/platform.js"
-import { formatBytes } from "@/utils/tools.js"
+import platformApi from "@/api/platform"
+import { formatBytes } from "@/utils/tools"
 import { CountTo } from "vue3-count-to"
 import { useI18n } from "vue-i18n"
-import { showSnackbar } from "@/utils/snackbar.js"
 
 
 const { t } = useI18n()
-const overviewData = ref({})
+
+const overviewData = ref({
+  runningTime: undefined,
+  memory: undefined,
+  roomCount: undefined,
+  worldCount: undefined,
+  userCount: undefined,
+  uidCount: undefined,
+  maxCpu: undefined,
+  maxMemory: undefined,
+  maxNetUp: undefined,
+  maxNetDown: undefined,
+})
+
 const overviewDataLoading = ref(false)
 
 const getOverviewData = () => {
