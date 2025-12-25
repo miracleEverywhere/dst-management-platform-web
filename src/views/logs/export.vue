67ed4248e8d70bdf34f2253501edfab99ff3db33
miracleEverywhere <763483966@qq.com>
2025-12-25 18:22:09 +0800

    添加日志下载页面

---
+++ src/views/logs/export.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn @click="downloadLog">
+    下载
+  </v-btn>
+</template>
+
+<script setup>
+import logsApi from "@/api/logs"
+import useGlobalStore from "@store/global.js"
+import { useDisplay } from "vuetify/framework"
+import { useI18n } from "vue-i18n"
+
+
+const globalStore = useGlobalStore()
+const { mobile } = useDisplay()
+const { t } = useI18n()
+
+const downloadLog = () => {
+  const reqForm = {
+    roomID: globalStore.room.id,
+  }
+
+  logsApi.download.download(reqForm, "logs.zip")
+}
+</script>
+
