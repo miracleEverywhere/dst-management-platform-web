2032f88ec21e34eccc9aa58d847cc79b5d1ebe83
miracleEverywhere <763483966@qq.com>
2026-01-15 10:45:54 +0800

    优化代码格式

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -180,16 +180,17 @@ export const timestamp2time = timestamp => {
 }
 
 export const isoTime2time = dateStr => {
-  const date = new Date(dateStr);
-  // 获取本地时间格式
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const date = new Date(dateStr)
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  // 获取本地时间格式
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
 
 export const timestamp2timeWithoutDate = timestamp => {
